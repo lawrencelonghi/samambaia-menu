c85a1259170cd6bcfaed7ad7e4648d439459aaa2
lawrencelonghi <lawlonghi@gmail.com>
2025-05-19 20:51:23 -0300

    categories update and scroll behavior fixed

--- public/script.js
+++ public/script.js
@@ -17,31 +17,3 @@ const btns = document.querySelectorAll('.nav-btn')
  })
 
 
- function getMenuItems() {
-  fetch('http://localhost:3000/menu')
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-        
-
-
-//       const menuList = document.querySelectorAll('.item'); 
-//       menuList.innerHTML = '';
-
-      
-//       data.forEach((item) => {
-//         const itemEl = document.createElement('div');
-//         itemEl.className = 'item';
-//         itemEl.innerHTML = `
-//           <div>
-//             <h2>${item.name}</h2>
-//             <p>${item.description}</p>
-//             <span>R$ ${item.price}</span>
-//           </div>
-//         `;
-//         menuList.appendChild(itemEl);
-//       });
-     })
-//     // .catch(error => console.error("Error loading menu:", error));
-}
-window.addEventListener('DOMContentLoaded', getMenuItems)
